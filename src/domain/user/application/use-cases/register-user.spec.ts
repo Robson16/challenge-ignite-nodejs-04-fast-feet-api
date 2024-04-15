8cd427c79a7e2669ea15d6328eede3e54fe30b82
Robson Henrique Rodrigues <robhenrod@gmail.com>
2024-04-15 12:52:11 -0300

    feat: register user tests
    - it should not be able to register with a invalid CPF;
    - it should not be able to register with CPF already in use;
    - it should not be able to register with Email already in use;

--- src/domain/user/application/use-cases/register-user.spec.ts
+++ src/domain/user/application/use-cases/register-user.spec.ts
@@ -1,5 +1,7 @@
 import { FakeHasher } from 'test/cryptography/fake-hasher'
 import { InMemoryUsersRepository } from 'test/repositories/in-memory-users-repository'
+import { InvalidCPFError } from './errors/invalid-cpf-error'
+import { UserAlreadyExistsError } from './errors/user-already-exists-error'
 import { RegisterUserUseCase } from './register-user'
 
 let inMemoryUsersRepository: InMemoryUsersRepository
@@ -42,4 +44,59 @@ describe('Register User', () => {
     expect(result.isRight()).toBe(true)
     expect(inMemoryUsersRepository.items[0].password).toEqual(hashedPassword)
   })
+
+  it('should not be able to register with an invalid CPF', async () => {
+    const result = await sut.execute({
+      name: 'John Doe',
+      cpf: 'invalid-cfp-number',
+      email: 'jonhdoe@example.com',
+      password: '123456',
+      role: 'DELIVERER',
+    })
+
+    expect(result.isLeft()).toBe(true)
+    expect(result.value).toBeInstanceOf(InvalidCPFError)
+  })
+
+  it('should not be able to register with CPF already in use', async () => {
+    await sut.execute({
+      name: 'Joe Six-Pack',
+      cpf: '267.859.975-26',
+      email: 'joesixpack@example.com',
+      password: '123456',
+      role: 'DELIVERER',
+    })
+
+    const result = await sut.execute({
+      name: 'John Doe',
+      cpf: '267.859.975-26',
+      email: 'jonhdoe@example.com',
+      password: '123456',
+      role: 'DELIVERER',
+    })
+
+    expect(result.isLeft()).toBe(true)
+    expect(result.value).toBeInstanceOf(UserAlreadyExistsError)
+  })
+})
+
+it('should not be able to register with Email already in use', async () => {
+  await sut.execute({
+    name: 'John Doe',
+    cpf: '667.316.070-53',
+    email: 'jonhdoe@example.com',
+    password: '123456',
+    role: 'DELIVERER',
+  })
+
+  const result = await sut.execute({
+    name: 'John Doe',
+    cpf: '267.859.975-26',
+    email: 'jonhdoe@example.com',
+    password: '123456',
+    role: 'DELIVERER',
+  })
+
+  expect(result.isLeft()).toBe(true)
+  expect(result.value).toBeInstanceOf(UserAlreadyExistsError)
 })
