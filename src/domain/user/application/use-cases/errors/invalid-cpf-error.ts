7321ef676e2e6e4d670ef22584afa066a0d8c8c9
Robson Henrique Rodrigues <robhenrod@gmail.com>
2024-04-17 14:10:43 -0300

    chore: just adjusting the final punctuation of the sentence

--- src/domain/user/application/use-cases/errors/invalid-cpf-error.ts
+++ src/domain/user/application/use-cases/errors/invalid-cpf-error.ts
@@ -2,6 +2,6 @@ import { UseCaseError } from '@/core/errors/use-case-error'
 
 export class InvalidCPFError extends Error implements UseCaseError {
   constructor() {
-    super('Invalid CPF')
+    super('Invalid CPF.')
   }
 }
--- src/domain/user/application/use-cases/errors/invalid-user-role.ts
+++ src/domain/user/application/use-cases/errors/invalid-user-role.ts
@@ -2,6 +2,6 @@ import { UseCaseError } from '@/core/errors/use-case-error'
 
 export class InvalidUserRole extends Error implements UseCaseError {
   constructor(role: string) {
-    super(`User role ${role} is invalid`)
+    super(`User role ${role} is invalid.`)
   }
 }
