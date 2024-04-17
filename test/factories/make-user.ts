63a035e80d4ebfe54222476afca9c531ae36f153
Robson Henrique Rodrigues <robhenrod@gmail.com>
2024-04-17 14:07:45 -0300

    refactor: in divided in two files for better separation of concerns

--- test/factories/make-user.ts
+++ test/factories/make-user.ts
@@ -1,15 +1,12 @@
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import { User, UserProps } from '@/domain/user/enterprise/entities/user'
 import { CPF } from '@/domain/user/enterprise/entities/value-objects/cpf'
-import { PrismaUserMapper } from '@/infra/database/prisma/mappers/prisma-user-mapper'
-import { PrismaService } from '@/infra/database/prisma/prisma.service'
 import { faker } from '@faker-js/faker'
-import { Injectable } from '@nestjs/common'
 
 export function makeUser(
   override: Partial<UserProps> = {},
   id?: UniqueEntityID,
-) {
+): User {
   const user = User.create(
     {
       name: faker.person.fullName(),
@@ -23,19 +20,4 @@ export function makeUser(
   )
 
   return user
-}
-
-@Injectable()
-export class UserFactory {
-  constructor(private prisma: PrismaService) {}
-
-  async makePrismaUser(data: Partial<UserProps> = {}): Promise<User> {
-    const user = makeUser(data)
-
-    await this.prisma.user.create({
-      data: PrismaUserMapper.toPrisma(user),
-    })
-
-    return user
-  }
 }
---
+++ test/factories/user-factory.ts
@@ -0,0 +1,20 @@
+import { User, UserProps } from '@/domain/user/enterprise/entities/user'
+import { PrismaUserMapper } from '@/infra/database/prisma/mappers/prisma-user-mapper'
+import { PrismaService } from '@/infra/database/prisma/prisma.service'
+import { Injectable } from '@nestjs/common'
+import { makeUser } from './make-user'
+
+@Injectable()
+export class UserFactory {
+  constructor(private prisma: PrismaService) {}
+
+  async makePrismaUser(data: Partial<UserProps> = {}): Promise<User> {
+    const user = makeUser(data)
+
+    await this.prisma.user.create({
+      data: PrismaUserMapper.toPrisma(user),
+    })
+
+    return user
+  }
+}
