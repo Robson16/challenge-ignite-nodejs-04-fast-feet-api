7f86f935f3f0052cb705bfd14a517f99d0aa501a
Robson Henrique Rodrigues <robhenrod@gmail.com>
2024-05-13 12:54:23 -0300

    feat: delete non-existent user test

--- src/domain/user/application/use-cases/delete-user.spec.ts
+++ src/domain/user/application/use-cases/delete-user.spec.ts
@@ -1,6 +1,7 @@
+import { ResourceNotFoundError } from '@/core/errors/resource-not-found-error'
+import { makeUser } from 'test/factories/make-user'
 import { InMemoryUsersRepository } from 'test/repositories/in-memory-users-repository'
 import { DeleteUserUseCase } from './delete-user'
-import { makeUser } from 'test/factories/make-user'
 
 let inMemoryUsersRepository: InMemoryUsersRepository
 let sut: DeleteUserUseCase // Subject Under Test
@@ -22,5 +23,14 @@ describe('Delete User', () => {
 
     expect(result.isRight()).toBe(true)
     expect(inMemoryUsersRepository.items).toHaveLength(0)
+  })
+
+  it('should not be able to delete a non-existent user', async () => {
+    const result = await sut.execute({
+      userId: 'non-existent-user-id',
+    })
+
+    expect(result.isLeft()).toBe(true)
+    expect(result.value).toBeInstanceOf(ResourceNotFoundError)
   })
 })
